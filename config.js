10a3836be7d3ec3744fe6e004a800678fbb002be
AngelBotsD <angelperez22448@gmail.com>
2026-01-02 01:34:31 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   nombrebot: 'Meow WaBot',
   moneda: '$ᴅᴏʟᴀʀᴇs',
-  apikey: '', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
+  apikey: 'AdonixKeybsh75y4198', // Pon tu apikey aqui, consiguela en: https://api-adonix.ultraplus.click
   prefijo: '.',
 
   owner: [
