fc132ec990191e8acf4f4f81b8d4d82601a09e69
AngelBotsD <angelperez22448@gmail.com>
2026-01-02 23:08:08 -0600

    Actualizar link.js

--- comandos/grupos/link.js
+++ comandos/grupos/link.js
@@ -2,11 +2,20 @@ async function handler(m, { conn }) {
   let code = await conn.groupInviteCode(m.chat)
   let link = 'https://chat.whatsapp.com/' + code
 
-  await conn.sendMessage(
-    m.chat,
-    { text: link, detectLink: true },
-    { quoted: m }
-  )
+  await conn.sendMessage(m.chat, {
+    text: link,
+    contextInfo: {
+      externalAdReply: {
+        title: '⚡ LINK DEL GRUPO ⚡',
+        body: 'Únete al grupo oficial',
+        thumbnailUrl: 'https://files.catbox.moe/xr2m6u.jpg',
+        sourceUrl: link,
+        mediaType: 1,
+        renderLargerThumbnail: true,
+        showAdAttribution: false
+      }
+    }
+  }, { quoted: m })
 }
 
 handler.command = ['link']
