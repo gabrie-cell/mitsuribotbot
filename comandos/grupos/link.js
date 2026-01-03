bf0c9bcac4b9b9d8987f9abef00d63a6f85175e2
AngelBotsD <angelperez22448@gmail.com>
2026-01-02 23:01:49 -0600

    Actualizar link.js

--- comandos/grupos/link.js
+++ comandos/grupos/link.js
@@ -1,8 +1,11 @@
-async function handler(m, { conn, orgs, participants, groupMetadata }) {
-  let group = m.chat;
-  let totalMembers = participants.length;
-  let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
-  conn.reply(m.chat, `*⚡🌩️──『 𝑳𝑰𝑵𝑲 』──🌩️⚡*
+async function handler(m, { conn, participants, groupMetadata }) {
+  let group = m.chat
+  let totalMembers = participants.length
+
+  let code = await conn.groupInviteCode(group)
+  let link = 'https://chat.whatsapp.com/' + code
+
+  let text = `*⚡🌩️──『 𝑳𝑰𝑵𝑲 』──🌩️⚡*
 
 📛 *Grupo:* ${groupMetadata.subject}
 👥 *Miembros:* ${totalMembers}
@@ -10,13 +13,19 @@ async function handler(m, { conn, orgs, participants, groupMetadata }) {
 🔗 *Enlace mágico:* 
 ${link}
 
-🐭 ¡Pikachu dice que lo compartas con los mejores entrenadores! ⚡`,  m, { detectLink: true });
+🐭 ¡Pikachu dice que lo compartas con los mejores entrenadores! ⚡`
+
+  await conn.sendMessage(
+    m.chat,
+    { text, detectLink: true },
+    { quoted: m }
+  )
 }
 
-handler.help = ['link'];
-handler.tags = ['grupo'];
-handler.command = ['link', 'enlace'];
-handler.group = true;
-handler.botAdmin = true;
+handler.help = ['link']
+handler.tags = ['grupo']
+handler.command = ['link', 'enlace']
+handler.group = true
+handler.botAdmin = true
 
-export default handler;
+export default handler
